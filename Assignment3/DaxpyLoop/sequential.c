7c461cfc0f479a4f807851b1433345ba1f1d1f0d
Rajas Ilamkar <113300283+RajasIlamkar@users.noreply.github.com>
2025-03-17 13:16:29 +0530

    Update sequential.c

--- Assignment3/DaxpyLoop/sequential.c
+++ Assignment3/DaxpyLoop/sequential.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
+#include <time.h>
 
-#define N (1 << 16)  // 65536 elements
+#define N (1 << 16)
+
+void daxpy(int n, double a, double *X, double *Y) {
+    for (int i = 0; i < n; i++) {
+        X[i] = a * X[i] + Y[i];
+    }
+}
 
 int main() {
-    double a = 3.5;
-    double *X = (double *)malloc(N * sizeof(double));
-    double *Y = (double *)malloc(N * sizeof(double));
-    
+    double *X, *Y;
+    double a = 2.5;
+    clock_t start, end;
+
+    // Allocate memory
+    X = (double *)malloc(N * sizeof(double));
+    Y = (double *)malloc(N * sizeof(double));
+
+    // Initialize vectors
     for (int i = 0; i < N; i++) {
         X[i] = 1.0;
         Y[i] = 2.0;
     }
 
-    double start_time = omp_get_wtime();
-    
-    for (int i = 0; i < N; i++) {
-        X[i] = a * X[i] + Y[i];
-    }
-    
-    double end_time = omp_get_wtime();
-    printf("Sequential Execution Time: %f seconds\n", end_time - start_time);
-    
+    // Start timer
+    start = clock();
+
+    // Perform DAXPY operation
+    daxpy(N, a, X, Y);
+
+    // End timer
+    end = clock();
+
+    printf("Serial Execution Time: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    // Free memory
     free(X);
     free(Y);
+
     return 0;
 }
